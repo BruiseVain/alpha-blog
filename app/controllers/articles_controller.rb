ee935e658f88cf235b0d7baa19521efb235c138f
Fredrik Dyngeland <Fredrik.Dyngeland@nhn.no>
2022-09-12 14:35:06 +0200

    Done with section 94 on Udemy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,38 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def find
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = "The article was created with great success!"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully mafakka."
+      redirect_to @article
+    else
+      render 'edit'
+    end
 
   end
+
 end
