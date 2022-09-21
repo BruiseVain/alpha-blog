399c4bc37c02c0d42403dc86cc37198416b3d171
Fredrik Dyngeland <Fredrik.Dyngeland@nhn.no>
2022-09-21 14:08:14 +0200

    Refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+  #Dette under blir da utført før noe annet skjer i controlleren, og kun for de i tabellen.
+  before_action :set_article, only: [:show,:edit,:update,:destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,11 +14,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
 
     if @article.save
       flash[:notice] = "The article was created with great success!"
@@ -27,14 +28,28 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully mafakka."
       redirect_to @article
     else
       render 'edit'
     end
 
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private #Alt under her er kun tilgjengelig i controlleren og ikke utenfor:
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
